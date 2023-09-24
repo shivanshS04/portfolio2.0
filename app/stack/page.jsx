909ba61059a7bc701b242c63fa8f3865465eb851
Shivansh Srivastava <shivanshsri.04@gmail.com>
2023-09-24 16:48:13 +0530

    stack added

--- app/stack/page.jsx
+++ app/stack/page.jsx
@@ -1,5 +1,13 @@
+import StackDiv from "@/components/StackDiv";
+import UpScroll from "@/components/scrollToTop";
+
 export default function page() {
     return (
-        <div>Stack</div>
+        <div className="lg:w-3/4 md:flex md:flex-col md:justify-center md:items-start lg:translate-x-40 w-full h-full p-10 mb-6 ">
+            <h1 className="scroll-m-20 text-4xl font-bold tracking-tight lg:text-5xl">Stack</h1>
+            <h4 className="leading-7 [&:not(:first-child)]:mt-6">Tools I use & love that you should definitely check out. 🪨⛏️</h4>
+            <StackDiv />
+            <UpScroll />
+        </div>
     )
 }
---
+++ components/scrollToTop.jsx
@@ -0,0 +1,14 @@
+'use client'
+import { ArrowUp } from 'lucide-react';
+
+export default function UpScroll() {
+    function scrollToTop() {
+        window.scrollTo({
+            top: 0,
+            behavior: 'smooth',
+        });
+    }
+    return (
+        <ArrowUp className="fixed bottom-0 right-0 m-4 max-sm:block md:hidden text-zinc-500 bg-zinc-800 p-1 rounded-sm" size={24} onClick={() => scrollToTop()} />
+    )
+}
